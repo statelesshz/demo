11c5ff3662e1ff3aabf0dffc5c89209465a58d41
unit_test <test@unit.com>
2024-02-29 10:45:45 +0800

    simple eval

---
+++ eleuther_eval.py
@@ -0,0 +1,54 @@
+import contextlib
+import os
+
+import transformers
+from lm_eval import evaluator
+from lm_eval.models.huggingface import HFLM
+from lm_eval.utils import make_table
+
+
+@contextlib.contextmanager
+def do_eval_with_ctx(*args, **kwargs):
+    # origin
+    origin_env_tokenizers_parallelism = os.environ.get("TOKENIZERS_PARALLELISM")
+    origin_get_model_info = HFLM.get_model_info
+    origin_get_git_commit_hash = evaluator.get_git_commit_hash
+    origin_AutoConfig = transformers.AutoConfig
+    origin_AutoTokenizer = transformers.AutoTokenizer
+    origin_AutoModelForCausalLM = transformers.AutoModelForCausalLM
+    origin_AutoModelForSeq2SeqLM = transformers.AutoModelForSeq2SeqLM
+
+    # patch
+    os.environ["TOKENIZERS_PARALLELISM"] = "0"
+    HFLM.get_model_info = lambda x: ""
+    evaluator.get_git_commit_hash = lambda : ""
+    
+    try:
+        result = evaluator.simple_evaluate(*args, **kwargs)
+        yield result
+    finally:
+        # rollback patch
+        if origin_env_tokenizers_parallelism is not None:
+            os.environ["TOKENIZERS_PARALLELISM"] = origin_env_tokenizers_parallelism
+        else:
+            del os.environ["TOKENIZERS_PARALLELISM"]
+        HFLM.get_model_info = origin_get_model_info
+        evaluator.get_git_commit_hash = origin_get_git_commit_hash
+
+
+def eval(*args, **kwargs):
+    with do_eval_with_ctx(
+        model="hf",
+        model_args="pretrained=qwen2-0.5b-instruct",
+        tasks=["arithmetic"],
+        batch_size=64,
+        device="npu:0",
+        limit=10,
+        **kwargs) as eval_result:
+        return eval_result
+
+results = eval()
+
+print(make_table(results))
+if "groups" in results:
+    print(make_table(results, "groups"))
