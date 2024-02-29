f054d041711dddc72e892cdb0af8c63587bfd60a
unit_test <test@unit.com>
2024-02-29 20:09:19 +0800

    update2

--- run.py
+++ run.py
@@ -1,26 +1,32 @@
 import os
-import sys
 from common import execute_subprocess_async
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 def run_text_classification():
     script_path = os.path.sep.join([
-        os.path.dirname(os.path.abspath(__file__)), "text-classification", "run_glue.py"
+        ROOT_PATH, "text-classification", "run_glue.py"
+    ])
+    model_path = "distilbert/distilbert-base-uncased"
+    # TODO: output_dir搞成能自动化创建&删除的形式
+    output_dir = os.path.sep.join([
+        ROOT_PATH, "tmp", model_path.split("/")[-1]
     ])
     cmd = [
         "torchrun",
         "--nproc_per_node=8",
         "--master_port=29501",
         script_path,
-        "--model_name_or_path=distilbert/distilbert-base-uncased",
-        "--output_dir=./tmp_text_classification",
+        "--model_name_or_path=" + model_path,
+        "--output_dir=" + output_dir,
         "--overwrite_output_dir",
         "--train_file=./data/mrpc/train-00000-of-00001.parquet",
         "--validation_file=./data/mrpc/validation-00000-of-00001.parquet",
         "--do_train",
         "--do_eval",
         "--per_device_train_batch_size=4",
-        "--per_device_eval_batch_size=2",
+        "--max_steps=2000",
         "--learning_rate=2e-5",
         "--report_to=none",
         "--max_seq_length=128",
