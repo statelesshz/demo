27a032197b5ec665989285eb739d798fc3de8869
statelesshz <hzji210@gmail.com>
2024-09-25 13:42:11 +0800

    demo.py

--- demo.py
+++ demo.py
@@ -1,20 +1,47 @@
 import os
+from pathlib import Path
+from typing import List
 
 
-model_dir = "/home/lynn/github/"
-
-is_local = os.path.isdir(model_dir)
-
-dir_entries = [f for f in os.listdir(model_dir)]
-
-if "config.json" in dir_entries:
-  print("backend: transformers")
-
-if "pytorch_model.bin" or "pytorch_model.bin.index.json" or "model.safetensors.index.json" or "model.safetensors" in dir_entries:
-  print("famework: torch")
-elif "mindspore_model.ckpt" or "mindspore_model.ckpt.index.json" in dir_entries:
-  print("framework: mindspore")
-
-# parse_model_metadata()
+def list_file_paths(model_dir):
+  # local version
+  return [str(p) for p in Path(model_dir).rglob("*")]
 
 
+def check_framework(paths: List[str]):
+  for path in paths:
+    # 先判断是不是pytorch格式的，主要考虑hf transformers diffusers结构
+    if path.endswith(".bin") or path.endswith(".safetensors"):
+      return "torch"
+
+  # fallback到mindspore
+  return "mindspore"
+
+
+def check_backend_and_model_type(paths: List[str], framework: str):
+  # 获取backend & model_type
+  if framework == "torch":
+    # 先看是不是diffusers
+    
+  elif framework == "mindspore":
+    ...
+  for path in paths:
+    if "diffusers" in path:
+      return "diffusers"
+
+
+def parse_model_metadata(model_name_or_path):
+  # 尽力而为的解析
+  is_local = os.path.isdir(model_name_or_path)
+
+  if is_local:
+    print("start scanning local file.")
+  else:
+    print("get model metadata from hub")
+    # model_repo
+
+
+if __name__ == "__main__":
+  # print(parse_model_metadata("/home/lynn/github/qwen2.5-0.5b-instruct"))
+  model_dir = "/home/lynn/github/qwen2.5-0.5b-instruct"
+  print(list_file_paths(model_dir))
